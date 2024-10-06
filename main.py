2049c9b933ce1066520e74dfba488d7a7a75e1c6
ParkRosie <rosiepark668@gmail.com>
2024-10-06 02:07:36 -0700

    commit4

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import keyword
 print("Welcome to Python!", "Fasiha" , sep="*", end=" ")
 print("We will learn interesting facts about Python today..")
 print('''Things that we will cover today Include:
@@ -10,3 +11,4 @@ print(b)
 print(a+b)
 msg=input('Enter Message:')
 print('Your message is: ', msg)
+print("The keywords are: ", keyword.kwlist)
