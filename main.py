306c056d2762071e3299c54edab6a2c8783703d1
ParkRosie <rosiepark668@gmail.com>
2024-10-06 01:49:19 -0700

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Welcome to Python!")
+print("Welcome to Python!", "Fasiha" , sep="*", end=" ")
+print("We will learn interesting facts about Python today..")
+print('''Things that we will cover today Include:
+                1.print command 
+                2.Variables
+                3.Formatting''')
